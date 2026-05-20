abc5cb7571418dc0f2793a0ad8155c971a969bca
Ashwin V <ASHWIN3351@gmail.com>
2026-05-20 06:04:36 +0000

    snapshot_and_custom_test

---
+++ snapshots/snap_orders.sql
@@ -0,0 +1,15 @@
+{% snapshot snap_raw_orders %}
+
+{{
+    config(
+        target_schema='snapshots',
+        unique_key='id',
+        strategy='timestamp',
+        updated_at='_etl_loaded_at',
+    )
+}}
+
+select *
+from {{ source('jaffle_shop', 'raw_orders') }}
+
+{% endsnapshot %}
