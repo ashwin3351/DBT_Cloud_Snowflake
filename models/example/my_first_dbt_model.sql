6f0f133065ab1336e4a11e2a76845ca5d7688949
Ashwin V <ASHWIN3351@gmail.com>
2026-05-26 06:55:13 +0000

    checklog

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -12,8 +12,8 @@
 {{
     config(
         materialized='table',
-        pre_hook="create table if not exists test_log (id int)",
-        post_hook="insert into test_log values (1)"
+        pre_hook="create table if not exists check_log (id int)",
+        post_hook="insert into check_log values (1)"
     )
 }}
 
